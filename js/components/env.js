b869576ac3a3099d18000b7b2231960fc66556c2
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-06-26 12:45:36 -0500

    chore: :package: Se cambiaron las keys

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-	'x-rapidapi-key': '531ff3b77fmsh9634bbefdc0ad9bp142abcjsnc26e357f34d9',
-		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+	'x-rapidapi-key': '97613e1bbfmshc66b008c486a9acp12cf65jsn14c87a648bfb',
+	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };
